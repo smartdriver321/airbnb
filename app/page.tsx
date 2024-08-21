b2567c922ae77279cfd53af283fa1ec02b883918
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-21 11:57:28 +0700

    Add MapFilterItems component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button'
+import { MapFilterItems } from './_components/MapFilterItems'
 
 export default function Home() {
 	return (
-		<div>
-			<Button className='bg-red-500'>Click Me!</Button>
+		<div className='container mx-auto px-5 lg:px-10'>
+			<MapFilterItems />
 		</div>
 	)
 }
