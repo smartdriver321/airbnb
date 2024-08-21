2f5438033c6c7dbd78eb18c055b8dd68cf79839c
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-21 11:14:18 +0700

    Add Kinde authentication

--- app/_components/UserNav.tsx
+++ app/_components/UserNav.tsx
@@ -1,5 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 import { MenuIcon } from 'lucide-react'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import {
+	RegisterLink,
+	LoginLink,
+	LogoutLink,
+} from '@kinde-oss/kinde-auth-nextjs/components'
 
 import {
 	DropdownMenu,
@@ -9,14 +15,17 @@ import {
 } from '@/components/ui/dropdown-menu'
 
 export async function UserNav() {
+	const { getUser } = getKindeServerSession()
+	const user = await getUser()
+
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger>
 				<div className='rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3'>
 					<MenuIcon className='w-6 h-6 lg:w-5 lg:h-5' />
-
 					<img
 						src={
+							user?.picture ??
 							'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg'
 						}
 						alt='Image of the user'
@@ -24,9 +33,24 @@ export async function UserNav() {
 					/>
 				</div>
 			</DropdownMenuTrigger>
+
 			<DropdownMenuContent className='w[1200px]' align='end'>
-				<DropdownMenuItem>Register</DropdownMenuItem>
-				<DropdownMenuItem>Login</DropdownMenuItem>
+				{user ? (
+					<>
+						<DropdownMenuItem>
+							<LogoutLink className='w-full'>Logout</LogoutLink>
+						</DropdownMenuItem>
+					</>
+				) : (
+					<>
+						<DropdownMenuItem>
+							<RegisterLink className='w-full'>Register</RegisterLink>
+						</DropdownMenuItem>
+						<DropdownMenuItem>
+							<LoginLink className='w-full'>Login</LoginLink>
+						</DropdownMenuItem>
+					</>
+				)}
 			</DropdownMenuContent>
 		</DropdownMenu>
 	)
