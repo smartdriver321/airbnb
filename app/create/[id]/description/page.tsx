4f78bbc08288dffae46d1283ddf718905f0faaf3
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-22 10:07:10 +0700

    Add Counter component to Description page

--- app/create/[id]/description/page.tsx
+++ app/create/[id]/description/page.tsx
@@ -1,3 +1,91 @@
-export default function Description() {
-	return <div>Hello from description page, you have added a category</div>
+import { Counter } from '@/app/_components/Counter'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+import { Card, CardHeader } from '@/components/ui/card'
+
+export default function Description({ params }: { params: { id: string } }) {
+	return (
+		<>
+			<div className='w-3/5 mx-auto'>
+				<h2 className='text-3xl font-semibold tracking-tight transition-colors'>
+					Please describe your home as good as you can!
+				</h2>
+			</div>
+
+			<form action=''>
+				<input type='hidden' name='homeId' value={params.id} />
+				<div className='mx-auto w-3/5 mt-10 flex flex-col gap-y-5 mb-36'>
+					<div className='flex flex-col gap-y-2'>
+						<Label>Title</Label>
+						<Input
+							name='title'
+							type='text'
+							required
+							placeholder='Short and simple...'
+						/>
+					</div>
+					<div className='flex flex-col gap-y-2'>
+						<Label>Description</Label>
+						<Textarea
+							name='description'
+							required
+							placeholder='Please describe your home...'
+						/>
+					</div>
+
+					<div className='flex flex-col gap-y-2'>
+						<Label>Price</Label>
+						<Input
+							name='price'
+							type='number'
+							required
+							placeholder='Price per night in USD'
+							min={10}
+						/>
+					</div>
+
+					<div className='flex flex-col gap-y-2'>
+						<Label>Image</Label>
+						<Input name='image' type='file' required />
+					</div>
+
+					<Card>
+						<CardHeader className='flex flex-col gap-y-5'>
+							<div className='flex items-center justify-between'>
+								<div className='flex flex-col'>
+									<h3 className='underline font-medium'>Guests</h3>
+									<p className='text-muted-foreground text-sm'>
+										How many guests do you want?
+									</p>
+								</div>
+
+								<Counter name='guest' />
+							</div>
+							<div className='flex items-center justify-between'>
+								<div className='flex flex-col'>
+									<h3 className='underline font-medium'>Rooms</h3>
+									<p className='text-muted-foreground text-sm'>
+										How many rooms do you have?
+									</p>
+								</div>
+
+								<Counter name='room' />
+							</div>
+							<div className='flex items-center justify-between'>
+								<div className='flex flex-col'>
+									<h3 className='underline font-medium'>Bathrooms</h3>
+									<p className='text-muted-foreground text-sm'>
+										How many bathrooms do you have?
+									</p>
+								</div>
+
+								<Counter name='bathroom' />
+							</div>
+						</CardHeader>
+					</Card>
+				</div>
+			</form>
+		</>
+	)
 }
